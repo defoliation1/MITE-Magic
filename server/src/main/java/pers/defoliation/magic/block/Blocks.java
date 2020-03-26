109ec12dac9f6af8d6263a1c680123ead4622a13
defoliation <defoli_ation@outlook.com>
2020-03-26 16:48:14 +0800

    update 1.0.0

--- server/src/main/java/pers/defoliation/magic/block/Blocks.java
+++ server/src/main/java/pers/defoliation/magic/block/Blocks.java
@@ -15,12 +15,12 @@ public class Blocks {
     public static void registerBlocks(){
         magicstoneOre = new MagicstoneOre(160, Items.magicstoneMaterial,4);
         magicMetalBlock = new MagicMetalBlock(161,Items.magicstoneMaterial);
-//        magicEnchantingTable = new MagicEnchantingTable(162,Items.magicstoneMaterial);
+        magicEnchantingTable = new MagicEnchantingTable(162,Items.magicstoneMaterial);
         magicBookshelf = new MagicBookshelf(163);
         magicAnvil = new MagicAnvil(164,Items.magicstoneMaterial);
         Item.g[magicstoneOre.cF] = new ItemBlock(magicstoneOre);
         Item.g[magicMetalBlock.cF] = new ItemBlock(magicMetalBlock);
-//        Item.g[magicEnchantingTable.cF] = new ItemBlock(magicEnchantingTable);
+        Item.g[magicEnchantingTable.cF] = new ItemBlock(magicEnchantingTable);
         Item.g[magicBookshelf.cF] = new ItemBlock(magicBookshelf);
         Item.g[magicAnvil.cF] = new ItemBlock(magicAnvil);
     }
